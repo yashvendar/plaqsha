7af591e128037cc172bdd22dabff5594392a5509
Yashvendar <yashvendarbadsiwal@gmail.com>
2021-03-06 21:08:01 +0530

    error resolved

--- src/components/useGAEventsTracker.jsx
+++ src/components/useGAEventsTracker.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga';
 
 
-const useGAEventsTracker = () => {
+const useGAEventsTracker = (category = 'Event Category') => {
     ReactGA.initialize('G-3J038KE39V')
     const trackEvent =({action='action', label ='label'})=>{
         ReactGA.event({category, action ,label})
